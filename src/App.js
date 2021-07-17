ffd1c1065195dbd9c1a985688c0356914d9bc81d
Letisia Pangata'a <lpan375@aucklanduni.ac.nz>
2021-07-18 05:30:03 +1200

    added files

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
+
+const App = () => {
+
+  const buttonColor = "#f2a33c";
+
+  return (
+    <div className="App">
+      <div className="calc-wrapper">
+        <Input />
+        <div className="row">
+          <Button symbol="7"/>
+          <Button symbol="8"/>
+          <Button symbol="9"/>
+          <Button symbol="/" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="4"/>
+          <Button symbol="5"/>
+          <Button symbol="6"/>
+          <Button symbol="*" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="1"/>
+          <Button symbol="2"/>
+          <Button symbol="3"/>
+          <Button symbol="+" color={buttonColor} />
+        </div>
+        <div className="row">
+          <Button symbol="0"/>
+          <Button symbol="."/>
+          <Button symbol="="/>
+          <Button symbol="-" color={buttonColor} />
+        </div>
+        <Button symbol="Clear" color="red"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
